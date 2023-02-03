78ecb2921f6b383c151ee5fa713fb03ca21b7a89
harishdadi1009@hotmail.com <harishdadi1009@hotmail.com>
2023-02-03 17:07:06 +0530

    all changes - add and mult

--- GSearchAutomation/src/main/java/GoogleSearchAutomate/GSearchAutomation/App.java
+++ GSearchAutomation/src/main/java/GoogleSearchAutomate/GSearchAutomation/App.java
@@ -7,13 +7,21 @@ package GoogleSearchAutomate.GSearchAutomation;
 public class App 
 {//commenting from master github
     int c=0;
+	int d=0;
+
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
                 
     }
+
     public void add(int a, int b){
             c=a+b;
             System.out.println("Addition of a and b is: "+c);
         }
+    
+    public void mult(int a, int b) {
+    	d=a*b;
+    	System.out.println("Multiplication of a and b is: "+d);
+    }
 }
