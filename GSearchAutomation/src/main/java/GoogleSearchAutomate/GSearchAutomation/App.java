8d7e806fe2d60407248b03388e704a84bde92d72
hdadi <71638889+hdadi@users.noreply.github.com>
2023-02-03 17:35:28 +0530

    Update App.java
    added division method from remote

--- GSearchAutomation/src/main/java/GoogleSearchAutomate/GSearchAutomation/App.java
+++ GSearchAutomation/src/main/java/GoogleSearchAutomate/GSearchAutomation/App.java
@@ -34,4 +34,9 @@ public class App
     	d=a*b*c;
     	System.out.println("Multiplication of a and b is: "+d);
     }
+	
+	public void divide(int a, int b) {
+    	d=a/b;
+    	System.out.println("Division of a and b is: "+d);
+    }
 }
