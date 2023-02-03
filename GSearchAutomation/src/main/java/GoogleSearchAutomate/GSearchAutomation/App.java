43ece3c5984282087e975ef59251602ff0f7ca7f
harishdadi1009@hotmail.com <harishdadi1009@hotmail.com>
2023-02-03 17:43:31 +0530

    added a method to calculate modulus of two numbers

--- GSearchAutomation/src/main/java/GoogleSearchAutomate/GSearchAutomation/App.java
+++ GSearchAutomation/src/main/java/GoogleSearchAutomate/GSearchAutomation/App.java
@@ -38,5 +38,10 @@ public class App
 	public void divide(int a, int b) {
     	d=a/b;
     	System.out.println("Division of a and b is: "+d);
+	}
+	public void mod(int a, int b) {
+    	d=a%b;
+    	System.out.println("Multiplication of a and b is: "+d);
+
     }
 }
