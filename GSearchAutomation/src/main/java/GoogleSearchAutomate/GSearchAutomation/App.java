6c5e505a1329c6aedc1ba529cd8c1144120bbd68
hdadi <71638889+hdadi@users.noreply.github.com>
2023-02-03 16:18:02 +0530

    Update App.java
    added a method for addition of two numbers

--- GSearchAutomation/src/main/java/GoogleSearchAutomate/GSearchAutomation/App.java
+++ GSearchAutomation/src/main/java/GoogleSearchAutomate/GSearchAutomation/App.java
@@ -6,8 +6,14 @@ package GoogleSearchAutomate.GSearchAutomation;
  */
 public class App 
 {//commenting from master github
+    int c=0;
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+                
     }
+    public int add(int a, int b){
+            c=a+b;
+            System.out.println("Addition of a and b is: "c);
+        }
 }
