d4999ea991d5b6ce90c7ee81e8604201ff3438ee
hdadi <71638889+hdadi@users.noreply.github.com>
2023-03-05 22:06:05 +0530

    added one more name display by Sysout

--- GSearchAutomation/src/test/java/testPackage/Test2.java
+++ GSearchAutomation/src/test/java/testPackage/Test2.java
@@ -7,7 +7,8 @@ public class Test2 {
 System.out.println("This is newly created class from Local1 branch from local");
 String[] names= {"Harish","Satish","Banu","Sudeep"};
 System.out.println("This is Mr."+names[0]);
-System.out.println("\nThis is Mr."+names[2]);		
+System.out.println("\nThis is Mr."+names[2]);	
+System.out.println("\nThis is Mr."+names[3]);	
 	}
 
 }
