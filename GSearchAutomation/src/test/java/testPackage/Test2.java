4d0d64dc0ef61bc8f812048b8bc72b4007c16559
harishdadi1009@hotmail.com <harishdadi1009@hotmail.com>
2023-03-05 22:00:10 +0530

    added names array in Test2.java

--- GSearchAutomation/src/test/java/testPackage/Test2.java
+++ GSearchAutomation/src/test/java/testPackage/Test2.java
@@ -5,6 +5,8 @@ public class Test2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("This is newly created class from Local1 branch from local");
+String[] names= {"Harish","Satish","Banu","Sudeep"};
+System.out.println("This is Mr."+names[0]);
 	}
 
 }
